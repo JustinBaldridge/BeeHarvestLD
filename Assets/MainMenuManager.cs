ae57197006e8d6491fca2011b74c9c366fc4a243
WillPower999 <williamcordelllegensworm@gmail.com>
2023-01-08 15:57:08 -0600

    Update MainMenuManager.cs

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -16,12 +16,6 @@ public class MainMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         loadGame.onClick.AddListener(LoadGame);
         exitApplication.onClick.AddListener(Exit);
         credits.onClick.AddListener(Credits);
