d757f6382db7e5dde2e0a40fbc7dd20d1dbbf6f8
WillPower999 <williamcordelllegensworm@gmail.com>
2023-01-08 21:56:35 -0600

    added sound manager, updated ui and tilesets, title screen

---
+++ Assets/Scripts/SoundDefinitions.cs
@@ -0,0 +1,23 @@
+public enum Sound
+{
+    buttonSFX = 0,
+    movementSFX,
+    waterSFX,
+    collectPollen,
+    collectNectar,
+    takeDamage,
+    bearSFX,
+    carSFX,
+    jsSFX,
+    levelComplete,
+    None
+}
+
+public enum Music
+{
+    title = 0,
+    game,
+    credits,
+    victory,
+    None
+}
