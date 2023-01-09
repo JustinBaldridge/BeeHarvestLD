bf5fbc8b770ad9a431566e3ae2cfc9ecb35f1014
JustinBaldridge <justinbaldridge@gmail.com>
2023-01-09 00:41:24 -0600

    stuff

--- Assets/Scripts/PulseAttack.cs
+++ Assets/Scripts/PulseAttack.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PulseAttack : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer sprite;
+
     [SerializeField] GameObject attackHitbox;
     [SerializeField] float attackTimer;
     [SerializeField] float attackDuration;
 
+    CharacterController player;
+
     float timer;
+
+    void Start()
+    {
+        player = FindObjectOfType<CharacterController>();
+    }
 
     void Update()
     {
@@ -21,6 +30,10 @@ public class PulseAttack : MonoBehaviour
         {
             timer += Time.deltaTime;
         }
+
+        Vector3 dir = player.gameObject.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        sprite.transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
     }
 
     IEnumerator ActivateAttack()
